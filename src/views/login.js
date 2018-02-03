e2d55aeb64301fbd6006ee54956930f4eb0b170c
Strigin Pavel <deep.dayver@gmail.com>
2018-02-03 11:31:07 +0400

    Верстка страницы авторизации

--- src/views/login.js
+++ src/views/login.js
@@ -1,12 +1,21 @@
 import React, {Component} from 'react'
 import {AUTH_URL} from '../constants'
 
+import logo from '../logo.jpg'
+
 export default class Home extends Component {
   render() {
     return (
-      <div className="login jumbotron">
-        <h1>Hello, please login!</h1>
-        <p><a href={AUTH_URL} className="btn btn-info btn-lg" role="button">Login</a></p>
+      <div className="page-header">
+        <div className="row">
+          <div className="col-md-4">
+            <img src={logo} alt="Логотип Яндек.Диск"/>
+          </div>
+          <div className="col-md-8 jumbotron">
+            <h2>Привет! <br/><small>Пожалуйста, нажми на кнопку</small></h2>
+            <p><a href={AUTH_URL} className="btn btn-info btn-lg" role="button">Войти</a></p>
+          </div>
+        </div>
       </div>
     );
   }
